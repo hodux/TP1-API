f6c7e21f29100761548c16ba4eeab702ff50b8e6
Mathieu Gouveia Sousa <2233635@collegemv.qc.ca>
2024-11-08 14:44:09 -0500

    render test 3-2

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,10 @@ import httpApp from './app'; // Importer l'application configurée
 const PORT = process.env.PORT || 3000;
 
 // Démarrer le serveur
-httpApp.listen(3000, '0.0.0.0');
+if (process.env.NODE_ENV == "prod") {
+    httpApp.listen(3000, '0.0.0.0');
+    console.log("Launching on HTTP for PROD")
+} else if (process.env.NODE_ENV == "dev") {
+
+    console.log("Launching on HTTPS for DEV")
+}
