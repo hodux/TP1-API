adf6028d94cb55493312c415eac71b22b0cec1a1
Mathieu Gouveia Sousa <2233635@collegemv.qc.ca>
2024-11-08 14:03:51 -0500

    render test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import httpApp from './app';  // Importer l'application configurée
 
 const PORT = process.env.PORT || 3000;
+const IP = process.env.IP || '0.0.0.0';
 
 // Démarrer le serveur
-httpApp.listen(PORT, () => {
-    console.log(`Serveur en écoute sur <https://localhost>:${PORT}`);
-});
+httpApp.listen(3000, '0.0.0.0');
